53335e82adb957de71f9f25381c584299702dd66
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-01-14 20:34:35 +0600

    Сделали компонент который получает информацию от Api исходя из того на какой странице мы находимся . Пока что с выводом информации в консоль

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Toolbar from "./components/ToolBar/Toolbar.tsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PageToShow from "./components/PageToShow/PageToShow.tsx";
 
 const App = () => (
     <>
         <header>
             <Toolbar/>
         </header>
+
+        <main className='container mt-5'>
+            <Routes>
+                <Route path="/" element={<Navigate to="/pages/about" />}/>
+                <Route path="/pages/:pageName" element={<PageToShow/>}/>
+                {/*<Route path="/pages/admin" element={<AdminPageEdit/>}/>*/}
+            </Routes>
+        </main>
     </>
 );
 
---
+++ src/components/PageToShow/PageToShow.tsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react';
+import { useParams } from "react-router-dom";
+import type {Page} from "../../types";
+import axiosApi from "../../axiosApi.ts";
+
+const PageToShow = () => {
+    const {pageName} = useParams();
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState<Page | null>(null);
+
+    useEffect(() => {
+        if (!pageName) return;
+
+        const getPage = async () => {
+            try {
+                setLoading(true);
+
+                const response = await axiosApi.get(`/pages/${pageName}.json`);
+                const responseData = await response.data;
+                setPage(responseData);
+                console.log(responseData)
+            } catch (e) {
+                console.log(e)
+            } finally {
+                setLoading(false);
+            }
+        }
+        getPage();
+    }, [pageName]);
+
+
+    return (
+        <div>
+            {loading && (
+                <div className="text-center my-3">
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="visually-hidden">Sending</span>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PageToShow;
